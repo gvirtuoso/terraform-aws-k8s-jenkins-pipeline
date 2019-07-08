add2189b9c0b992fe68440cc3cf54ebb9381d8f2
Guilherme Virtuoso <guilherme@shawandpartners.com>
2019-07-08 02:58:46 -0300

    Upgrading configuration files to be suitable for Terraform v0.12

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,55 +1,83 @@
 output "region" {
-  value = "${data.aws_region.current.name}"
+  value = data.aws_region.current.name
 }
 
 output "vpc_id" {
-  value = "${module.vpc.vpc_id}"
+  value = module.vpc.vpc_id
 }
 
 output "vpc_name" {
-  value = "${local.vpc_name}"
+  value = local.vpc_name
 }
 
 output "vpc_cidr_block" {
-  value = "${module.vpc.vpc_cidr_block}"
+  value = module.vpc.vpc_cidr_block
 }
 
 output "public_subnet_ids" {
-  value = ["${module.vpc.public_subnets}"]
+  value = module.vpc.public_subnets
 }
 
 output "public_route_table_ids" {
-  value = ["${module.vpc.public_route_table_ids}"]
+  value = module.vpc.public_route_table_ids
 }
 
 output "private_subnet_ids" {
-  value = ["${module.vpc.private_subnets}"]
+  value = module.vpc.private_subnets
 }
 
 output "private_route_table_ids" {
-  value = ["${module.vpc.private_route_table_ids}"]
+  value = module.vpc.private_route_table_ids
 }
 
 output "default_security_group_id" {
-  value = "${module.vpc.default_security_group_id}"
+  value = module.vpc.default_security_group_id
 }
 
 output "nat_gateway_ids" {
-  value = "${module.vpc.natgw_ids}"
+  value = module.vpc.natgw_ids
 }
 
 output "availability_zones" {
-  value = ["${local.vpc_azs}"]
+  value = local.vpc_azs
+}
+
+output "k8s_version" {
+  value = local.k8s_version
 }
 
 output "k8s_cluster_name" {
-  value = "${local.k8s_cluster_name}"
+  value = local.k8s_cluster_name
+}
+
+output "k8s_master_instance_type" {
+  value = local.k8s_master_instance_type
+}
+
+output "k8s_master_instance_min" {
+  value = local.k8s_master_instance_min
+}
+
+output "k8s_master_instance_max" {
+  value = local.k8s_master_instance_max
+}
+
+output "k8s_node_instance_type" {
+  value = local.k8s_node_instance_type
+}
+
+output "k8s_node_instance_min" {
+  value = local.k8s_node_instance_min
+}
+
+output "k8s_node_instance_max" {
+  value = local.k8s_node_instance_max
 }
 
 output "k8s_common_http_sg_id" {
-  value = "${aws_security_group.k8s_common_http.id}"
+  value = aws_security_group.k8s_common_http.id
 }
 
 output "kops_s3_bucket" {
-  value = "${aws_s3_bucket.kops_state.bucket}"
+  value = aws_s3_bucket.kops_state.bucket
 }
--- terraform/virtuoso-security-groups.tf
+++ terraform/virtuoso-security-groups.tf
@@ -1,23 +1,23 @@
 resource "aws_security_group" "k8s_common_http" {
-    name   = "${local.application}-k8s-common-http-sg-${local.environment}"
-    vpc_id = "${module.vpc.vpc_id}"
-    tags = "${merge(
-        local.tags,
-        map(
-            "Name", "${local.application}-k8s-common-http-sg-${local.environment}"
-        )
-    )}"
+  name   = "${local.application}-k8s-common-http-sg-${local.environment}"
+  vpc_id = module.vpc.vpc_id
+  tags = merge(
+    local.tags,
+    {
+      "Name" = "${local.application}-k8s-common-http-sg-${local.environment}"
+    },
+  )
 
-    ingress {
-        protocol    = "tcp"
-        from_port   = 80
-        to_port     = 80
-        cidr_blocks = "${local.k8s_ingress_ips}"
-    }
-    ingress {
-        protocol    = "tcp"
-        from_port   = 443
-        to_port     = 443
-        cidr_blocks = "${local.k8s_ingress_ips}"
-    }
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = local.k8s_ingress_ips
+  }
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = local.k8s_ingress_ips
+  }
 }
