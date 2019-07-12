c3c028daac5a4e23c3653b16ee0297eb134a09c8
Guilherme Virtuoso <guilherme@shawandpartners.com>
2019-07-12 04:37:29 -0300

    Updating outputs on the shell scripts

--- 100.kops-validate-cluster.sh
+++ 100.kops-validate-cluster.sh
@@ -8,6 +8,9 @@ TF_OUTPUT=$(cd terraform && terraform output -json)
 CLUSTER_NAME="$(echo ${TF_OUTPUT} | jq -r .k8s_cluster_name.value)"
 STATE="s3://$(echo ${TF_OUTPUT} | jq -r .kops_s3_bucket.value)"
 
+echo "Cluster Name...: ${CLUSTER_NAME}"
+echo "Kops State.....: ${STATE}"
+
 echo "===================================================="
 echo "Validating cluster creation"
 echo "===================================================="
