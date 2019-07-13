2bfb0545cbcbcb099060a8590db9a478d08ad0ec
Guilherme Virtuoso <guilherme@shawandpartners.com>
2019-07-13 16:25:01 -0300

    Renaming file and configuring helm

--- 003.k8s-config.sh
+++ 003.k8s-config.sh
@@ -16,3 +16,10 @@ echo "Generating Kubectl configuration"
 echo "===================================================="
 kops export kubecfg --name ${CLUSTER_NAME} --state ${STATE}
 kubectl config set-cluster ${CLUSTER_NAME} --server=https://api.${CLUSTER_NAME}
+
+echo "===================================================="
+echo "Configuring helm"
+echo "===================================================="
+kubectl -n kube-system create serviceaccount tiller
+kubectl create clusterrolebinding tiller --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller
